f3f04fda4545d9511db801236c0303453336383c
ssggg12 <mpcm5@naver.com>
2024-11-22 15:50:57 +0900

    Add files via upload

---
+++ week12_ex_02.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+	int temp;
+	printf("temp = %d\n", temp);
+	return 0;
+}
+
---
+++ week12_main.c_ex_12.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+extern void f2();
+
+int main(void)
+{
+	f2();
+	return 0;
+}
